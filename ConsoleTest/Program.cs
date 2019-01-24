34e2b99405b7ad882682161dad6cc8850aca0bde
wei1071427874 <1071427874@qq.com>
2019-01-24 13:52:23 +0800

    测试2

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleTest.weiyong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        /// <summary>
+        /// 调用代码单独写在自己的文件夹中
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Ts.Test();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleTest/weiyong/ts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.weiyong
+{
+    internal static class Ts
+    {
+        public static void Test()
+        {
+            Console.WriteLine("测试");
+        }
+    }
+}
