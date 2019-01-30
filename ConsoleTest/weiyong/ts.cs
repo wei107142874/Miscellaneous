354fb7f500637794f1c68a59e56ac576b06c3f99
魏勇 <1071427874@qq.com>
2019-01-30 17:41:32 +0800

    1

--- ConsoleTest/weiyong/ts.cs
+++ ConsoleTest/weiyong/ts.cs
@@ -9,10 +9,28 @@ namespace ConsoleTest.weiyong
 {
     internal static class Ts
     {
+        public static int sl = 0;
+
         public static void Test()
         {
-           CommHelper.UseQuartz2();
+            //CommHelper.UseQuartz2();
+            Console.WriteLine(shuliang(0));
             Console.WriteLine("成功");
+        }
+
+       
+        public static int shuliang(int n)
+        {
+            if (n == 6)
+            {
+                sl=5;
+            }
+            else
+            {
+                sl = (shuliang(n + 1) + 1) * 2;
+                Console.WriteLine(sl);
+            }
+            return sl;
         }
     }
 }
